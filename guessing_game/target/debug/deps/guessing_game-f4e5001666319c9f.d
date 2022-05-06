e0c422e486504d015fa6ce2967c184a27f0d34b4
Taeha Hong <xogk39@gmail.com>
2022-05-06 16:00:43 +0900

    Step1. get input data

---
+++ guessing_game/target/debug/deps/guessing_game-f4e5001666319c9f.d
@@ -0,0 +1,5 @@
+/Users/taehahong/Desktop/Dongtae/rust_practice/guessing_game/target/debug/deps/guessing_game-f4e5001666319c9f: src/main.rs
+
+/Users/taehahong/Desktop/Dongtae/rust_practice/guessing_game/target/debug/deps/guessing_game-f4e5001666319c9f.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/taehahong/Desktop/Dongtae/rust_practice/guessing_game/target/debug/guessing_game: /Users/taehahong/Desktop/Dongtae/rust_practice/guessing_game/src/main.rs
